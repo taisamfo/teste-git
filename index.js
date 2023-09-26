ebcc48bd5436500080329a6f3c32e04d54a7cac6
Taisa <taisaformiga@hotmail.com>
2023-09-25 21:37:28 -0300

    novo

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-console.log("Olá mundo")
-console;log("hello node")
+// Solicita ao usuário que insira o nome e a experiência (XP) do herói
+const nomeHeroi = prompt("Digite o nome do herói: ");
+const experienciaHeroi = parseInt(prompt("Digite a quantidade de experiência (XP) do herói: "));
 
-let ligado = true
-if(ligado){
-console.log("Executou") 
+let nivel;
+
+// Define o nível do herói com base na experiência (XP)
+if (experienciaHeroi < 1000) {
+  nivel = "Ferro";
+} else if (experienciaHeroi >= 1000 && experienciaHeroi <= 2000) {
+  nivel = "Bronze";
+} else if (experienciaHeroi >= 2001 && experienciaHeroi <= 5000) {
+  nivel = "Prata";
+} else if (experienciaHeroi >= 6001 && experienciaHeroi <= 7000) {
+  nivel = "Ouro";
+} else if (experienciaHeroi >= 7001 && experienciaHeroi <= 8000) {
+  nivel = "Platina";
+} else if (experienciaHeroi >= 8001 && experienciaHeroi <= 9000) {
+  nivel = "Ascendente";
+} else if (experienciaHeroi >= 9001 && experienciaHeroi <= 10000) {
+  nivel = "Imortal";
 } else {
-console.log("luzes desligaram")
+  nivel = "Radiante";
 }
+
+// Exibe a mensagem com o nome e o nível do herói
+alert(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
